8a88065a54d01d849df15bd96d17a8eb0337785e
Robert Lowe <pngwen@gmail.com>
2025-09-25 01:38:43 +0000

    first completed version

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file main.cpp
+ * @author Robert Lowe <rlowe8@utm.edu> 
+ * @brief Test sorting algorithms for speed.
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "sortTest.hpp"
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+    unsigned int arsize;
+    unsigned int runcount=10;
+
+    // Display usage 
+    if(argc != 2 and argc != 3) {
+        std::cerr << "Usage: " << argv[0] 
+                  << " <array size> [<run count>]" << std::endl;
+        return -1;
+    }
+
+    // get the arsize and runcount
+    arsize = static_cast<unsigned int>(std::strtoul(argv[1], nullptr, 10));
+    if(argc == 3) {
+        runcount = static_cast<unsigned int>(std::strtoul(argv[1], nullptr, 10));
+    }
+
+    // handle invalid arguments 
+    if(arsize == 0 or runcount == 0) {
+        std::cerr << "Invalid arguments." << std::endl;
+        return -2;
+    }
+
+    // First, run the validity test
+    if(not validityTest(arsize)) {
+        std::cerr << "Validity test failed. Aborting." << std::endl;
+        return -3;
+    }
+
+    // Now run the timing tests
+    timeTest(arsize, runcount);
+}
